2724fabbb0a2e9967b7290749310481b5d1cffed
minseok <micheal117@pusan.ac.kr>
2025-06-12 19:29:57 +0900

    refactor(LogItem.kt)
    파이어 베이스 데이터 추가

--- app/src/main/java/com/example/nutriscanner/log/LogItem.kt
+++ app/src/main/java/com/example/nutriscanner/log/LogItem.kt
@@ -1,7 +1,15 @@
 package com.example.nutriscanner.log
 
+import android.os.Parcelable
+import com.example.nutriscanner.result.Food
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class LogItem(
     val id: String,
     val dateTime: String,
-    val imageUri: String
-)
+    val imageUri: String,
+    val foods: List<Food>,
+    val totalNutrients: Map<String, Double>,
+    val gptComment: String
+) : Parcelable
