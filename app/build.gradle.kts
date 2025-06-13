0c51ffc13fb2336f9ca1a48591b861e2d8d72d9d
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-06-13 11:32:51 +0900

    refactor(build.gradle.kts)
    api 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ dependencies {
 
     implementation("org.tensorflow:tensorflow-lite:2.7.0")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
