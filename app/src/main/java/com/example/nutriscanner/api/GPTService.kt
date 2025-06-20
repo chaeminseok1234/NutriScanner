53bb94d77318768ad6797d51a16b5ae70b1852a8
minseok <micheal117@pusan.ac.kr>
2025-06-20 17:26:18 +0900

    refactor(GPTService.kt)
    주석 수정

--- app/src/main/java/com/example/nutriscanner/api/GPTService.kt
+++ app/src/main/java/com/example/nutriscanner/api/GPTService.kt
@@ -6,6 +6,7 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
+//POST 요청으로 OpenAI GPT에 대화를 요청하고, 영양 분석 결과를 응답받는 함수
 interface GPTService {
     @POST("chat/completions")
     fun getNutritionFeedback(@Body request: ChatRequest): Call<ChatResponse>
